f69fb0df6fd7894768768785d18dd4bf9777ec6b
wattanx <wattan.dev@gmail.com>
2024-09-16 21:45:45 +0900

    fix: re-rendering

--- src/lib/Island.ts
+++ src/lib/Island.ts
@@ -1,4 +1,5 @@
-import { watch, computed, createStaticVNode, defineComponent, getCurrentInstance, onMounted, ref, createVNode } from 'vue';
+import type { RendererNode } from 'vue';
+import { watch, computed, createStaticVNode, defineComponent, getCurrentInstance, h, ref, createVNode } from 'vue';
 import { hash } from 'ohash';
 import { randomUUID } from 'uncrypto';
 import { joinURL, withQuery } from 'ufo';
@@ -35,13 +36,14 @@ export default defineComponent({
   emits: ['error'],
   async setup(props, { slots, emit }) {
     const teleportKey = ref(0);
-    const key = ref(0);
+
     const error = ref<unknown>(null);
     const instance = getCurrentInstance()!;
     const hashId = computed(() => hash([props.name, props.props, props.context]));
 
-    const html = ref('');
+    const html = ref(import.meta.client ? getFragmentHTML(instance?.vnode?.el).join('') ?? '<div></div>' : '<div></div>');
 
+    const key = ref(0);
     async function _fetchComponent() {
       const key = `${props.name}_${hashId.value}`;
 
@@ -54,27 +56,26 @@ export default defineComponent({
         })
       );
 
+      // @ts-expect-error
       const result = import.meta.server || !import.meta.dev ? await r.json() : r._data;
 
       return result;
     }
 
-    const nuxtApp = {} as Record<string, any>;
+    const ssrApp = {} as Record<string, any>;
 
     async function fetchComponent() {
-      nuxtApp[pKey] = nuxtApp[pKey] || {};
-      // TODO: support ssr
-      if (!nuxtApp[pKey][hashId.value]) {
-        console.log('server render', import.meta.server);
-        nuxtApp[pKey][hashId.value] = _fetchComponent().finally(() => {
+      ssrApp[pKey] = ssrApp[pKey] || {};
+
+      if (!ssrApp[pKey][hashId.value]) {
+        ssrApp[pKey][hashId.value] = _fetchComponent().finally(() => {
           // eslint-disable-next-line @typescript-eslint/no-dynamic-delete
-          delete nuxtApp[pKey]![hashId.value];
+          delete ssrApp[pKey]![hashId.value];
         });
       }
 
       try {
-        const res = await nuxtApp[pKey][hashId.value];
-        console.log('res', res);
+        const res = await ssrApp[pKey][hashId.value];
 
         key.value++;
         error.value = null;
@@ -98,7 +99,7 @@ export default defineComponent({
 
     if (import.meta.client && !instance.vnode.el && props.lazy) {
       fetchComponent();
-    } else if (import.meta.server || !instance.vnode.el || !nuxtApp.payload.serverRendered) {
+    } else if (import.meta.server || !instance.vnode.el) {
       await fetchComponent();
     }
 
@@ -106,6 +107,44 @@ export default defineComponent({
       return () => createVNode('div');
     }
 
-    return () => createStaticVNode(html.value, 1);
+    return () =>
+      h(
+        (_, { slots }) => slots.default?.(),
+        { key: key.value },
+        {
+          default: () => [createStaticVNode(html.value, 1)],
+        }
+      );
   },
 });
+
+function getFragmentHTML(element: RendererNode | null) {
+  if (element) {
+    if (element.nodeName === '#comment' && element.nodeValue === '[') {
+      return getFragmentChildren(element);
+    }
+    return [element.outerHTML];
+  }
+  return [];
+}
+
+function getFragmentChildren(element: RendererNode | null, blocks: string[] = []) {
+  if (element && element.nodeName) {
+    if (isEndFragment(element)) {
+      return blocks;
+    } else if (!isStartFragment(element)) {
+      blocks.push(element.outerHTML);
+    }
+
+    getFragmentChildren(element.nextSibling, blocks);
+  }
+  return blocks;
+}
+
+function isStartFragment(element: RendererNode) {
+  return element.nodeName === '#comment' && element.nodeValue === '[';
+}
+
+function isEndFragment(element: RendererNode) {
+  return element.nodeName === '#comment' && element.nodeValue === ']';
+}

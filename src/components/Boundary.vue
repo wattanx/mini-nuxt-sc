5594c8e6750bb195c3074850550416129d9dfa5a
wattanx <wattan.dev@gmail.com>
2024-09-29 23:16:12 +0900

    chore

--- src/components/Boundary.vue
+++ src/components/Boundary.vue
@@ -14,7 +14,7 @@ withDefaults(defineProps<Props>(), {
     class="p-6 rounded-md relative border-dashed border-gray-400 border"
     :class="{
       'bg-red-900': isClient,
-      'bg-green-900': !isClient,
+      'bg-blue-900': !isClient,
     }"
   >
     <p class="absolute -top-4 bg-gray-600 px-4 py-2 rounded-full text-sm">

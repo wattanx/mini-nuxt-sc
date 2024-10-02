8c2c8c24f9c61d514b12b0784b14b79619cfdfc5
wattanx <wattan.dev@gmail.com>
2024-10-02 23:18:27 +0900

    refactor

--- src/entry-island.js
+++ src/entry-island.js
@@ -1,9 +1,9 @@
 import { createApp } from 'vue';
-import App from './IslandApp.vue';
+import IslandRenderer from './lib/IslandRenderer';
 import { renderToString } from 'vue/server-renderer';
 
 export async function render(ssrContext = {}) {
-  const app = createApp(App, { ssrContext });
+  const app = createApp(IslandRenderer, { context: ssrContext.islandContext });
 
   const html = await renderToString(app, ssrContext);
 

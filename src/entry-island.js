a0e2b2bcbdf8804602751085e13039f1f65aa1d6
wattanx <wattan.dev@gmail.com>
2024-10-06 15:59:00 +0900

    fix

--- src/entry-island.js
+++ src/entry-island.js
@@ -1,9 +1,9 @@
-import { createApp } from 'vue';
+import { createSSRApp } from 'vue';
 import IslandRenderer from './lib/IslandRenderer';
 import { renderToString } from 'vue/server-renderer';
 
 export async function render(ssrContext = {}) {
-  const app = createApp(IslandRenderer, { context: ssrContext.islandContext });
+  const app = createSSRApp(IslandRenderer, { context: ssrContext.islandContext });
 
   const html = await renderToString(app, ssrContext);
 
